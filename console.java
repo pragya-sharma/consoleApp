3119fa0b17e51491a90cc0b45f93f4d07f9c84cf
pragya-sharma <45152090+pragya-sharma@users.noreply.github.com>
2021-05-13 13:14:29 +0530

    Create console.java

---
+++ console.java
@@ -0,0 +1,73 @@
+import java.util.*;
+class finalExam{
+    private String make;
+    private String model;
+    private int year;
+    private String color;
+    private String price;
+
+    public finalExam(String make, String model,int year,String color,String price) {
+               this.make = make;
+               this.model = model;
+               this.year = year;
+               this.color = color;
+               this.price = price;
+    }
+    public String getMake() {
+        return make;
+    }
+
+    // Setter
+    public void setMake(String newMake) {
+        this.make = newMake;
+    }
+
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String newModel){
+        this.model = newModel;
+    }
+    public int getYear(){
+        return year;
+    }
+    public void setYear(int newYear){
+        this.year = newYear;
+    }
+    public String getColor(){
+        return color;
+    }
+    public  void  setColor(String newColor){
+        this.color = newColor;
+    }
+    public  String getPrice(){
+        return price;
+    }
+    public void setPrice(String newPrice){
+        this.price = newPrice;
+    }
+    @Override
+    public String toString(){
+        return getMake()+" " + getModel()+ " " +getYear()+" "+ getColor()+ " " +getPrice();
+    }
+    
+}
+
+
+
+
+public class console  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String make = sc.nextLine();
+        String model = sc.next();
+        int year = sc.nextInt();
+        String color = sc.next();
+        String price = sc.next();
+        finalExam objj = new finalExam(make, model, year, color, price);
+
+
+        System.out.println(objj);
+    }
+
+}
